11f6d2a6bd1b1cb9ee72614bfcaa7602927c0756
pledgedavis <pledgedavis14@gmail.com>
2020-01-09 22:22:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  attr_accessor :roster 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+def add_student(name, grade)
+  if 
+    @roster[grade]
+   @roster[grade] << name
+ else 
+   @roster[grade] = [name]
+ end
+  
+   def grade(grade)
+    @roster[grade]
+  
+  
+end
+  def sort
+  empty_hash ={}
+  empty_hash[@name] = grade
+  
+  
+end
+end
+end
+
+
+
+
