07d81e4e02cfdf0ad514d25f8b89ab5b3d9aea17
pledgedavis <pledgedavis14@gmail.com>
2020-01-09 22:32:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+  attr_accessor :roster 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+def add_student(name, grade)
+  if 
+    @roster[grade]
+   @roster[grade] << name
+ else 
+   @roster[grade] = [name]
+ end
+  
+   def grade(grade)
+    @roster[grade]
+  
+  
+end
+  def sort
+    # binding.pry
+  # empty_hash ={}
+  @roster.each do |grade, names|
+  @roster[grade] = names.sort
+  
+end
+end
+end
+end
+
+
+
+
